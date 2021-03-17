a6c2e1b740c98e08f0ced40e03ff1c67d4cfd1e5
Jackie <taekwondo.chan@gmail.com>
2021-03-16 18:16:50 -0700

    navigate through links

--- scrapy_test.py
+++ scrapy_test.py
@@ -7,7 +7,14 @@ class RepSpider(scrapy.Spider):
 
     def parse(self, response):
         print('-------------------------------------------')
-        data = response.xpath("//ol//strong/a/@href")
-        for item in data:
-            print(item.extract())
+        links = response.xpath("//ol//strong/a/@href")
+        link_list = []
+        for link in links:
+            link_list.append(link.extract())
+
+        next_page = response.xpath("//ol//strong/a/@href").get()
+
+        for page in link_list:
+            print(f'navigating through link: {next_page}')
+            yield scrapy.Request(page, callback=self.parse)
         print('-------------------------------------------')
