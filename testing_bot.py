340efdda22d139ec5464c41206d7195258674b45
Jackie <taekwondo.chan@gmail.com>
2021-03-16 21:52:59 -0700

    testing stock detection

--- testing_bot.py
+++ testing_bot.py
@@ -65,6 +65,34 @@ def scrape(category):
     return names_prices
 
 
+def check_olympic_plates():
+    url = 'https://www.roguefitness.com/rogue-calibrated-lb-steel-plates'
+    while True:
+        try:
+            proxy = proxy_generator()
+            print(f"Proxy: {proxy}")
+            headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+            r = requests.get(url, proxies=proxy, timeout=3, headers=headers)
+            break
+        except:
+            print('Trying new proxy')
+            pass
+
+    print('Found working proxy')
+    page_content = bs(r.content, features='html5lib')
+    names = page_content.select('div.item-name')
+    grouped_items = page_content.select('div.grouped-item-row')
+    stock_status = []
+
+    for item in grouped_items:
+        if item.select('div.item-qty.input-text'):
+            stock_status.append(1)
+        else:
+            stock_status.append(0)
+
+    results = {names[i].string: stock_status[i] for i in range(len(names))}
+    return results
+
 @bot.command()
 async def ping(ctx):
     print('Command recieved: !ping')
@@ -81,6 +109,14 @@ async def bars(ctx):
     for name, price in values.items():
         await ctx.channel.send(f'{name}: {price}')
     await ctx.channel.send('------------------')
-    
 
+@bot.command()
+async def check_plates(ctx):
+    values = check_olympic_plates()
+    for name, stock_status in values.items():
+        if stock_status == 1:
+            await ctx.channel.send(f'{name}: In stock!')
+        else:
+            await ctx.channel.send(f'{name}: OOS!')
+    
 bot.run(TOKEN)
