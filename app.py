ad4b6e322759b67d745d46f86d6901a4ae04fdff
Jackie <taekwondo.chan@gmail.com>
2021-03-16 17:21:59 -0700

    begin returning dicts

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import requests
 from random import choice
 
 def proxy_generator():
-    response = requests.get("https://sslproxies.org/")
-    soup = bs(response.content, 'html5lib')
+    r = requests.get("https://sslproxies.org/")
+    soup = bs(r.content, 'html5lib')
 
     proxy_list = soup.find_all('td')[::8]
     ports_list = soup.find_all('td')[1::8]
@@ -23,8 +23,10 @@ def proxy_generator():
 
     return proxy
 
-def scrape_bumpers():
-    url = 'https://www.roguefitness.com/weightlifting-bars-plates/bumpers/competition-bumpers'
+def scrape():
+
+    # Can be changed to any category url
+    url = 'https://www.roguefitness.com/weightlifting-bars-plates'
 
     while True:
         try:
@@ -38,15 +40,17 @@ def scrape_bumpers():
             pass
 
     page_content = bs(r.content, features='html5lib')
-    all_products = page_content.select('h2.product-name')
+    all_product_names = page_content.select('h2.product-name')
+    all_product_prices = page_content.select('span.price')
 
-    names = []
+    names_prices = {}
 
-    for product in all_products:
-        product_name = product.find('a').string
-        names.append(product_name)
+    i = 0
+    while i < len(all_product_names):
+        product_name = (all_product_names[i]).find('a').string
+        names_prices[f'{product_name}'] = (all_product_prices[i]).string
+        i+=1
 
-    print(names)
+    return names_prices
 
-
-scrape_bumpers()
+scrape()
